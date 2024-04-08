a21b7d2166a8e2634c66fe2b5d86874e4685b4ff
Daniel <abraodaniel@hotmail.com>
2024-04-08 14:10:22 -0300

    adjusting reports per group page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,18 +2,24 @@ class ReportsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    # @group = Group.find(params[:group_id])
-    # @operations = Operation.all.where("group_id = ?", @group.id).order(created_at: :desc) rescue nil
+    @report_types = [{"name" => "Visão geral de transações", "source" => "generate_all_values"}, 
+    {"name" => "Valor de transações por categoria", "source" => "values_per_groups"}
+    ]
   end
 
   def generate_operation_report
     start_date = params[:start_date].values.first
     end_date = params[:end_date].values.first
+    report_type = params[:report_type]
     total_amount_value = 0
     if start_date.present? && end_date.present?
-      total_amount_value = Operation.get_total_operations_in_range(start_date, end_date,  current_user.id)
+      if report_type == 'generate_all_values'
+        total_amount_value = Operation.get_total_operations_in_range(start_date, end_date,  current_user.id)
+        redirect_to reports_path, flash: { total_revenue: total_amount_value[:total_revenue], total_spent: total_amount_value[:total_spent], start_date: start_date, end_date: end_date}
+      elsif report_type == 'values_per_groups'
+        report_values = Operation.get_total_operations_per_group(start_date, end_date,  current_user.id)
+        redirect_to reports_path, flash: { report_values: report_values, start_date: start_date, end_date: end_date}
+      end
     end
-
-    redirect_to reports_path, flash: { total_revenue: total_amount_value[:total_revenue], total_spent: total_amount_value[:total_spent], start_date: start_date, end_date: end_date}
   end
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -24,4 +24,19 @@ class Operation < ApplicationRecord
     end
     return {total_revenue: total_revenue_amount, total_spent: total_spent_amount}
   end
+
+  def self.get_total_operations_per_group start_date, end_date, current_user_id
+    transactions_per_group = self.select("
+          operations.group_id,
+          groups.name as group_name,
+          groups.icon as group_icon,
+          SUM(CASE WHEN operation_type = 0 THEN amount ELSE 0 END) AS total_revenue,
+          SUM(CASE WHEN operation_type = 1 THEN amount ELSE 0 END) AS total_spent
+        ")
+        .joins("JOIN groups ON operations.group_id = groups.id")
+        .where("author_id = ? AND DATE(operations.created_at) BETWEEN ? AND ?", current_user_id, start_date, end_date)
+        .group("operations.group_id, groups.id")
+    report_data = transactions_per_group.map(&:attributes)
+    return {report_data: report_data}
+  end
 end
