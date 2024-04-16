f8f312bae43ddaa2d03e08a43cd4dbd08337e3f2
Daniel <abraodaniel@hotmail.com>
2024-04-16 15:20:46 -0300

    adjusting forms and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,16 +12,16 @@ class UsersController < ApplicationController
 
   # GET /users/new
   def new
-    raise root_path.inspect
     @user = User.new
   end
 
   # GET /users/1/edit
-  def edit; end
+  def edit; 
+    @user = User.find(params[:id])
+  end
 
   # POST /users or /users.json
   def create
-    raise root_path.inspect
     @user = User.new(user_params)
 
     respond_to do |format|
@@ -36,16 +36,14 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+  def update_user
+    @user = User.find(params[:id])
+    if params[:user]['password'].present? && params[:user]['password_confirmation'].present? && params[:user]['password'] == params[:user]['password_confirmation']
+      @user.update(user_params)
+    else
+      @user.update(name: params[:user]['name'], email: params[:user]['email'])
     end
+    redirect_to user_path(@user.id)
   end
 
   # DELETE /users/1 or /users/1.json
@@ -67,6 +65,6 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
